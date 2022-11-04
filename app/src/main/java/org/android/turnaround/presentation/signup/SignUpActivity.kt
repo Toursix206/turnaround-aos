05474d3055ef1c8a03a66573ad090aedd7ff676a
Yunjoo Son <thsduswn4141@naver.com>
2022-11-04 23:58:08 +0900

    #1 [ui] 회원가입 레이아웃

---
+++ app/src/main/java/org/android/turnaround/presentation/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package org.android.turnaround.presentation.signup
+
+import android.os.Bundle
+import org.android.turnaround.R
+import org.android.turnaround.databinding.ActivitySignUpBinding
+import org.android.turnaround.util.binding.BindingActivity
+
+class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
