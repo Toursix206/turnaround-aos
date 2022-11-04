6696ba0fedbd6de9cd16558a2c1482f9ca02b4fd
Yunjoo Son <thsduswn4141@naver.com>
2022-11-04 17:55:34 +0900

    #1 [ui] 튜토리얼 레이아웃

---
+++ app/src/main/java/org/android/turnaround/presentation/tutorial/TutorialActivity.kt
@@ -0,0 +1,12 @@
+package org.android.turnaround.presentation.tutorial
+
+import android.os.Bundle
+import org.android.turnaround.R
+import org.android.turnaround.databinding.ActivityTutorialBinding
+import org.android.turnaround.util.binding.BindingActivity
+
+class TutorialActivity : BindingActivity<ActivityTutorialBinding>(R.layout.activity_tutorial) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
