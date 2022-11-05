2ea1a64b3d87f3d61fed11c68ffa366d32bd9a95
Yunjoo Son <thsduswn4141@naver.com>
2022-11-05 15:50:30 +0900

    #2 [feat] 스플래시 > 튜토리얼 화면 이동 구현

--- app/src/main/java/org/android/turnaround/presentation/intro/IntroActivity.kt
+++ app/src/main/java/org/android/turnaround/presentation/intro/IntroActivity.kt
@@ -1,12 +1,27 @@
 package org.android.turnaround.presentation.intro
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import org.android.turnaround.R
+import org.android.turnaround.presentation.tutorial.TutorialActivity
+
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+        moveToTutorial()
+    }
+
+    private fun moveToTutorial() {
+        // 추후 Lottie 보여주고 화면이동 시킬 것임
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            val intent = Intent(this, TutorialActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
