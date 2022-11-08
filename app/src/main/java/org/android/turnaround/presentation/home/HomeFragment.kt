3ee74f725e70eb93839a13844ba299e99a3b2cd4
Yunjoo Son <thsduswn4141@naver.com>
2022-11-09 03:26:27 +0900

    #10 [add] 홈, 방타버스, 활동, 마이 4개의 화면 생성 및 아이콘 추가

---
+++ app/src/main/java/org/android/turnaround/presentation/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package org.android.turnaround.presentation.home
+
+import android.os.Bundle
+import android.view.View
+import org.android.turnaround.R
+import org.android.turnaround.databinding.FragmentHomeBinding
+import org.android.turnaround.util.binding.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
+
