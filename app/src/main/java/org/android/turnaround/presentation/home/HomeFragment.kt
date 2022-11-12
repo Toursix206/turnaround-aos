c79d912176b0be4232398b75bfabe3be555f5ca2
minyoung <minwachya@gmail.com>
2022-11-12 11:07:15 +0900

    #13 [feat] 홈 그림자 제외 레이아웃 구현

--- app/src/main/java/org/android/turnaround/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/android/turnaround/presentation/home/HomeFragment.kt
@@ -2,12 +2,53 @@ package org.android.turnaround.presentation.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.viewpager2.widget.ViewPager2
 import org.android.turnaround.R
 import org.android.turnaround.databinding.FragmentHomeBinding
+import org.android.turnaround.domain.entity.*
+import org.android.turnaround.presentation.home.adapter.TodoAdapter
 import org.android.turnaround.util.binding.BindingFragment
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initHomeActivityAdapter()
+    }
+
+    private fun initHomeActivityAdapter() {
+        with(binding.vpHomeTodo) {
+            adapter = TodoAdapter().apply {
+                val testList = listOf<Todo>(
+                    NoTodo("민영"),
+                    TodoWhite("침대", "졸리다", "10시까지", R.drawable.ic_home_broom),
+                    TodoBlack("침대", "자야죠", R.drawable.ic_main_my),
+                    TodoPurple("화장실", "청소는 실어", R.drawable.ic_home_broom)
+                )
+                submitHomeActivityList(testList)
+
+                val pageMargin = resources.getDimension(R.dimen.vp_home_page_margin)
+                val pagerOffset = resources.getDimension(R.dimen.vp_home_pager_offset)
+                setShowSideItems(pageMargin, pagerOffset)
+            }
+        }
+    }
+
+    private fun ViewPager2.setShowSideItems(pageMarginPx: Float, offsetPx: Float) {
+        offscreenPageLimit = 3
+
+        setPageTransformer { page, position ->
+            val offset = position * -(2 * offsetPx + pageMarginPx)
+            if (this.orientation == ViewPager2.ORIENTATION_HORIZONTAL) {
+                if (ViewCompat.getLayoutDirection(this) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+                    page.translationX = -offset
+                } else {
+                    page.translationX = offset
+                }
+            } else {
+                page.translationY = offset
+            }
+        }
     }
 }
---
+++ app/src/main/java/org/android/turnaround/presentation/home/adapter/TodoAdapter.kt
@@ -0,0 +1,97 @@
+package org.android.turnaround.presentation.home.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.android.turnaround.databinding.ItemNoTodoBinding
+import org.android.turnaround.databinding.ItemTodoBlackBinding
+import org.android.turnaround.databinding.ItemTodoPurpleBinding
+import org.android.turnaround.databinding.ItemTodoWhiteBinding
+import org.android.turnaround.domain.entity.*
+
+class TodoAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val todoList = mutableListOf<Todo>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        return when (viewType) {
+            VIEW_TYPE_NO_TODO -> NoTodoViewHolder(ItemNoTodoBinding.inflate(inflater, parent, false))
+            VIEW_TYPE_TODO_BLACK -> TodoBlackViewHolder(ItemTodoBlackBinding.inflate(inflater, parent, false))
+            VIEW_TYPE_TODO_PURPLE -> TodoPurpleViewHolder(ItemTodoPurpleBinding.inflate(inflater, parent, false))
+            else -> TodoWhiteViewHolder(ItemTodoWhiteBinding.inflate(inflater, parent, false)) // VIEW_TYPE_PRODUCT
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is NoTodoViewHolder -> {
+                val item = todoList[position] as NoTodo
+                holder.bind(item)
+            }
+            is TodoWhiteViewHolder -> {
+                val item = todoList[position] as TodoWhite
+                holder.bind(item)
+            }
+            is TodoBlackViewHolder -> {
+                val item = todoList[position] as TodoBlack
+                holder.bind(item)
+            }
+            is TodoPurpleViewHolder -> {
+                val item = todoList[position] as TodoPurple
+                holder.bind(item)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = todoList.size
+
+    override fun getItemViewType(position: Int): Int {
+        return when (todoList[position]) {
+            is NoTodo -> VIEW_TYPE_NO_TODO
+            is TodoWhite -> VIEW_TYPE_TODO_WHITE
+            is TodoBlack -> VIEW_TYPE_TODO_BLACK
+            is TodoPurple -> VIEW_TYPE_TODO_PURPLE
+        }
+    }
+
+    fun submitHomeActivityList(items: List<Todo>) {
+        todoList.addAll(items)
+        notifyItemRangeInserted(items.size, items.size)
+    }
+
+    class NoTodoViewHolder(private val binding: ItemNoTodoBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(todo: NoTodo) {
+            binding.todo = todo
+            binding.executePendingBindings()
+        }
+    }
+
+    class TodoWhiteViewHolder(private val binding: ItemTodoWhiteBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(todo: TodoWhite) {
+            binding.todo = todo
+            binding.executePendingBindings()
+        }
+    }
+
+    class TodoBlackViewHolder(private val binding: ItemTodoBlackBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(todo: TodoBlack) {
+            binding.todo = todo
+            binding.executePendingBindings()
+        }
+    }
+
+    class TodoPurpleViewHolder(private val binding: ItemTodoPurpleBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(todo: TodoPurple) {
+            binding.todo = todo
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object {
+        private const val VIEW_TYPE_NO_TODO = 0
+        private const val VIEW_TYPE_TODO_WHITE = 1
+        private const val VIEW_TYPE_TODO_BLACK = 2
+        private const val VIEW_TYPE_TODO_PURPLE = 3
+    }
+}
