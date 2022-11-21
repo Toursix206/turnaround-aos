90aa24427ed975a742320ba7ffbe484c8bce27f4
Yunjoo Son <thsduswn4141@naver.com>
2022-11-21 19:46:09 +0900

    #33 [ui] 활동시작, 활동완료 화면 레이아웃

---
+++ app/src/main/java/org/android/turnaround/presentation/start_todo/StartTodoActivity.kt
@@ -0,0 +1,12 @@
+package org.android.turnaround.presentation.start_todo
+
+import android.os.Bundle
+import org.android.turnaround.R
+import org.android.turnaround.databinding.ActivityStartTodoBinding
+import org.android.turnaround.util.binding.BindingActivity
+
+class StartTodoActivity : BindingActivity<ActivityStartTodoBinding>(R.layout.activity_start_todo) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
