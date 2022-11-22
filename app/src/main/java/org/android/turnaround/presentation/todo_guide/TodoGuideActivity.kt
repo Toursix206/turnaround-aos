3a544ebb539489c916f7e851820132335b39fdd4
Yunjoo Son <thsduswn4141@naver.com>
2022-11-22 18:37:31 +0900

    #34 [feat] 활동 가이드 : 사진 뷰페이저 동작 구현 (유저 클릭 막음)

--- app/src/main/java/org/android/turnaround/presentation/todo_guide/TodoGuideActivity.kt
+++ app/src/main/java/org/android/turnaround/presentation/todo_guide/TodoGuideActivity.kt
@@ -5,15 +5,44 @@ import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.android.turnaround.R
 import org.android.turnaround.databinding.ActivityTodoGuideBinding
+import org.android.turnaround.presentation.todo_guide.adapter.TodoGuideAdapter
 import org.android.turnaround.util.binding.BindingActivity
+import org.android.turnaround.util.extension.repeatOnStarted
 
 @AndroidEntryPoint
 class TodoGuideActivity : BindingActivity<ActivityTodoGuideBinding>(R.layout.activity_todo_guide) {
     private val viewModel by viewModels<TodoGuideViewModel>()
+    private val todoGuideAdapter = TodoGuideAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = viewModel
         viewModel.getTodoGuide(1)
+        initGuideImgViewPager()
+        initGuidesCollector()
+        initCurrentStepCollector()
+    }
+
+    private fun initGuideImgViewPager() {
+        with(binding.vpTodoGuide) {
+            adapter = todoGuideAdapter
+            isUserInputEnabled = false
+        }
+    }
+
+    private fun initGuidesCollector() {
+        repeatOnStarted {
+            viewModel.guides.collect { guides ->
+                todoGuideAdapter.submitList(guides)
+            }
+        }
+    }
+
+    private fun initCurrentStepCollector() {
+        repeatOnStarted {
+            viewModel.currentStep.collect { step ->
+                binding.vpTodoGuide.currentItem = step - 1
+            }
+        }
     }
 }
