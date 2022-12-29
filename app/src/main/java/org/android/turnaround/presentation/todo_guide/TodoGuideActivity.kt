f31d2ac6d9597ef41150383f8ae6f502b4f18ee0
Yunjoo Son <thsduswn4141@naver.com>
2022-12-29 16:32:32 +0900

    #43 [feat] 카메라 권한 체크 및 카메라 사용 후 인증사진 가져오기 구현

--- app/src/main/java/org/android/turnaround/presentation/todo_guide/TodoGuideActivity.kt
+++ app/src/main/java/org/android/turnaround/presentation/todo_guide/TodoGuideActivity.kt
@@ -1,22 +1,51 @@
 package org.android.turnaround.presentation.todo_guide
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import dagger.hilt.android.AndroidEntryPoint
 import org.android.turnaround.R
 import org.android.turnaround.databinding.ActivityTodoGuideBinding
+import org.android.turnaround.presentation.todo_certificate.TodoCertificateActivity
 import org.android.turnaround.presentation.todo_guide.adapter.TodoGuideAdapter
 import org.android.turnaround.util.binding.BindingActivity
 import org.android.turnaround.util.dialog.DialogBtnClickListener
 import org.android.turnaround.util.dialog.WarningDialogFragment
 import org.android.turnaround.util.dialog.WarningType
 import org.android.turnaround.util.extension.repeatOnStarted
+import org.android.turnaround.util.getImgUri
+import org.android.turnaround.util.getPathFromUri
+import timber.log.Timber
+import java.io.File
 
 @AndroidEntryPoint
 class TodoGuideActivity : BindingActivity<ActivityTodoGuideBinding>(R.layout.activity_todo_guide) {
     private val viewModel by viewModels<TodoGuideViewModel>()
     private val todoGuideAdapter = TodoGuideAdapter()
+    private var imgUri: Uri? = null
+    private val fromCameraActivityLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        imgUri?.let { uri ->
+            Thread.sleep(700)
+            if (File(getPathFromUri(this, uri)).exists()) {
+                startActivity(
+                    Intent(this, TodoCertificateActivity::class.java).apply {
+                        putExtra(IMG_URI, uri.toString())
+                    }
+                )
+                finish()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +54,7 @@ class TodoGuideActivity : BindingActivity<ActivityTodoGuideBinding>(R.layout.act
         initGuideImgViewPager()
         initCloseBtnClickListener()
         initCloseToolTipBtnClickListener()
+        initDoneBtnClickListener()
         initGuidesCollector()
         initCurrentStepCollector()
     }
@@ -63,6 +93,57 @@ class TodoGuideActivity : BindingActivity<ActivityTodoGuideBinding>(R.layout.act
         }
     }
 
+    private fun initDoneBtnClickListener() {
+        binding.btnTodoGuideDone.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                if (checkCameraPermission()) {
+                    takePicture()
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(android.Manifest.permission.CAMERA),
+                        REQUEST_CAMERA_PERMISSION
+                    )
+                }
+            } else {
+                if (checkCameraPermissionUnderQ()) {
+                    takePicture()
+                } else {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(
+                            android.Manifest.permission.CAMERA,
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ),
+                        REQUEST_CAMERA_PERMISSION_UNDER_Q
+                    )
+                }
+            }
+        }
+    }
+
+    private fun checkCameraPermission() =
+        ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+
+    private fun checkCameraPermissionUnderQ() =
+        checkCameraPermission() && ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun takePicture() {
+        try {
+            imgUri = getImgUri(contentResolver)
+                ?: throw NullPointerException()
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
+                it.putExtra(MediaStore.EXTRA_OUTPUT, imgUri)
+                fromCameraActivityLauncher.launch(it)
+            }
+        } catch (e: NullPointerException) {
+            Timber.tag("ProfileBottomSheet").e("ImgUri Null 에러")
+        }
+    }
+
     private fun initGuidesCollector() {
         repeatOnStarted {
             viewModel.guides.collect { guides ->
@@ -81,5 +162,8 @@ class TodoGuideActivity : BindingActivity<ActivityTodoGuideBinding>(R.layout.act
 
     companion object {
         const val TODO_GUIDE_TODO_ID = "todoGuideTodoId"
+        const val IMG_URI = "imageUri"
+        const val REQUEST_CAMERA_PERMISSION = 2
+        const val REQUEST_CAMERA_PERMISSION_UNDER_Q = 3
     }
 }
---
+++ app/src/main/java/org/android/turnaround/util/ImageProvider.kt
@@ -0,0 +1,42 @@
+package org.android.turnaround.util
+
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+
+fun getImgUri(contentResolver: ContentResolver): Uri? {
+    val folderName = "Spark"
+    val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+    val picturePath = "${Environment.DIRECTORY_PICTURES}${File.separator}$folderName"
+    val contentValues = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, "$timeStamp.jpeg")
+        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            put(MediaStore.MediaColumns.RELATIVE_PATH, picturePath)
+        }
+    }
+
+    return contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+}
+
+fun getPathFromUri(context: Context, uri: Uri): String {
+    val cursor: Cursor = context.contentResolver.query(uri, null, null, null, null)
+        ?: throw NullPointerException()
+    cursor.moveToNext()
+    val columnIndex = cursor.getColumnIndex("_data")
+    val path = if (columnIndex >= 0) {
+        cursor.getString(columnIndex)
+    } else {
+        throw IllegalAccessException()
+    }
+    cursor.close()
+    return path
+}
