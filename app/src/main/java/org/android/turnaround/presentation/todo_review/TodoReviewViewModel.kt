dd92fe4e0b333a506b8ae9a3ee97ad1cb2a90a23
Yunjoo Son <thsduswn4141@naver.com>
2023-01-03 23:12:28 +0900

    #43 [feat] 활동 리뷰 작성 - 조회 시 서버로부터 받아올 데이터들 바인딩 작업

--- app/src/main/java/org/android/turnaround/presentation/todo_review/TodoReviewViewModel.kt
+++ app/src/main/java/org/android/turnaround/presentation/todo_review/TodoReviewViewModel.kt
@@ -1,5 +1,20 @@
 package org.android.turnaround.presentation.todo_review
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import org.android.turnaround.domain.entity.NotWrittenReview
 
-class TodoReviewViewModel : ViewModel()
+class TodoReviewViewModel : ViewModel() {
+    val reviewContent = MutableStateFlow(EMPTY_CONTENT)
+    val reviewRating = MutableStateFlow(DEFAULT_RATING)
+
+    private val _notWrittenReview = MutableStateFlow(NotWrittenReview())
+    val notWrittenReview: StateFlow<NotWrittenReview> = _notWrittenReview.asStateFlow()
+
+    companion object {
+        const val EMPTY_CONTENT = ""
+        const val DEFAULT_RATING = 0f
+    }
+}
