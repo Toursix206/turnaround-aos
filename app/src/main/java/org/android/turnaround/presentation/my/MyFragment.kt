d30a61a405513689d1edb428023fc97cd6fe4db1
Yunjoo Son <thsduswn4141@naver.com>
2023-01-09 00:46:16 +0900

    #77 [feat] 마이페이지 : 스토어 리뷰 남기기 -> 구글 플레이 스토어 연결

--- app/src/main/java/org/android/turnaround/presentation/my/MyFragment.kt
+++ app/src/main/java/org/android/turnaround/presentation/my/MyFragment.kt
@@ -1,6 +1,7 @@
 package org.android.turnaround.presentation.my
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
@@ -16,14 +17,22 @@ class MyFragment : BindingFragment<FragmentMyBinding>(R.layout.fragment_my) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.vm = viewModel
         initStartSettingBtnClickListener()
+        initReviewClickListener()
     }
 
     private fun initStartSettingBtnClickListener() {
         binding.tvMySetting.setOnClickListener {
             startActivity(Intent(requireActivity(), SettingActivity::class.java))
+        }
+    }
+
+    private fun initReviewClickListener() {
+        binding.tvMyReview.setOnClickListener {
+            startActivity(
+                Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.market_url)))
+            )
         }
     }
 }
