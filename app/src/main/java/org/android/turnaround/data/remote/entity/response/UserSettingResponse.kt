7a2d25d8c6cfd19c60a0c3b278b88b3ea9082d0c
Yunjoo Son <thsduswn4141@naver.com>
2023-01-10 22:09:18 +0900

    #79 [fix] 마이페이지 설정 : 서버 통신 수정 부분 적용

--- app/src/main/java/org/android/turnaround/data/remote/entity/response/UserSettingResponse.kt
+++ app/src/main/java/org/android/turnaround/data/remote/entity/response/UserSettingResponse.kt
@@ -1,14 +1,24 @@
 package org.android.turnaround.data.remote.entity.response
 
 import com.google.gson.annotations.SerializedName
+import org.android.turnaround.domain.entity.OpenSourceLicenseUrl
 import org.android.turnaround.domain.entity.UserSetting
 
 data class UserSettingResponse(
     @SerializedName("agreeAllPushNotification")
-    val isAgreeNotification: Boolean
+    val isAgreeNotification: Boolean,
+    val openSourceLicenseUrl: OpenSourceLicenseUrlEntity,
+    @SerializedName("privateInformationPolicyUrl")
+    val policyUrl: String
 ) {
     fun toUserSetting() =
         UserSetting(
-            isAgreeNotification = this.isAgreeNotification
+            isAgreeNotification = this.isAgreeNotification,
+            openSourceLicenseUrl = OpenSourceLicenseUrl(this.openSourceLicenseUrl.aos),
+            policyUrl = this.policyUrl
         )
 }
+
+data class OpenSourceLicenseUrlEntity(
+    val aos: String
+)
