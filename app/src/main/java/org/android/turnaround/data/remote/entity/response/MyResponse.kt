1b00d50eca58c47f78ac0d419d777486bde46c99
Yunjoo Son <thsduswn4141@naver.com>
2023-01-10 22:27:51 +0900

    #79 [ui] 팝업 제목 온점 삭제

--- app/src/main/java/org/android/turnaround/data/remote/entity/response/MyResponse.kt
+++ app/src/main/java/org/android/turnaround/data/remote/entity/response/MyResponse.kt
@@ -1,5 +1,6 @@
 package org.android.turnaround.data.remote.entity.response
 
+import com.google.gson.annotations.SerializedName
 import org.android.turnaround.domain.entity.My
 import org.android.turnaround.domain.entity.ProfileType
 import org.android.turnaround.domain.entity.StoreUrl
@@ -9,6 +10,7 @@ data class MyResponse(
     val nickname: String,
     val point: Int,
     val csUrl: String,
+    @SerializedName("storeUrlInfo")
     val storeUrl: StoreUrlEntity
 ) {
     fun toMy(): My =
